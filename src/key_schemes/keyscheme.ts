a807f2556901319e36313527fa9ed78842dab517
Jeremy Elbourn <jelbourn@google.com>
2019-04-12 15:24:04 -0700

    Add KeyScheme interface

---
+++ src/key_schemes/keyscheme.ts
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+
+/**
+ * Represents a set of keyboard controls that map to actions. One or more KeySchemes can
+ * be applied to a control.
+ *
+ * @template T Control type for which this key scheme applies. Typically a union of one or
+ *     more behaviors.
+ */
+export interface KeyScheme<T> {
+  /**
+   * Handles a key press, returning whether the given key mapped to an action.
+   * @param control The control instance for which some action may be taken.
+   * @param keyEvent The keyboard event being handled.
+   * @returns Whether the key event has handled by this scheme.
+   */
+  handleKey(control: T, keyEvent: KeyboardEvent): boolean;
+}
