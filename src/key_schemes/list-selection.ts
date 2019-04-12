f3fed17ab4219b5d392cbbfdc3ec0ee891fd6920
Jeremy Elbourn <jelbourn@google.com>
2019-04-12 15:37:05 -0700

    Add ListSelectionKeyScheme

--- src/key_schemes/list-selection.ts
+++ src/key_schemes/list-selection.ts
@@ -5,3 +5,22 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
+import {CanBeSelected, HasSelectedDescendant} from '../behaviors/behavior-interfaces';
+import {SPACE} from './keycodes';
+import {KeyScheme} from './keyscheme';
+
+
+/** Key scheme for selecting items with a list-like control, such as a listbox. */
+export class ListSelectionKeyScheme implements KeyScheme<HasSelectedDescendant<CanBeSelected>> {
+  handleKey(control: HasSelectedDescendant<CanBeSelected>, event: KeyboardEvent) {
+    const keyCode = event.keyCode;
+    switch (keyCode) {
+      case SPACE:
+        control.toggleActiveItemSelection();
+        return true;
+    }
+
+    return false;
+  }
+}
